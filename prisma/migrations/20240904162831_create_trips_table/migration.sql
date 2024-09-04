a9250e0d285c0f87bac3920542be8b4944bf4ed3
thbueno <thiagobueno.s@gmail.com>
2024-09-05 01:11:20 +0700

    Initial Commit

---
+++ prisma/migrations/20240904162831_create_trips_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "trips" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "destination" TEXT NOT NULL,
+    "starts_at" DATETIME NOT NULL,
+    "ends_at" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import fastify from "fastify";
+import { prisma } from "./lib/prisma";
+
+const app = fastify()
+
+app.get('/cadastrar', async() => {
+    await prisma.trip.create({
+        data: {
+            destination: 'Camboja',
+            starts_at: new Date(),
+            ends_at: new Date(),
+        }
+    })
+
+    return 'Registro cadastrado com sucesso!'
+})
+
+app.get('/listar', async() => {
+    const trips = await prisma.trip.findMany()
+
+    return trips
+})
+
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('server Running!')
+})
